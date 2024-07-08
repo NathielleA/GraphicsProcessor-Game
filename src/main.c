447d513a3c824b02a1c1b2f31503347bbd5bb70c
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-07-08 16:08:45 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,7 +13,9 @@
 //-------------------------------------------------------------
 
 int main(void) {
-  set_game_sprite();
+  set_game_sprites();
+
+  set_background_color(1, 1, 1);
 
   sprite_fixed_t frog;
   frog.ativo = 1;
@@ -21,6 +23,19 @@ int main(void) {
   frog.coord_y = 100;
   frog.data_register = 1;
   frog.offset = 0;
+
+  set_fixed_sprite(frog);
+
+  //-----------------------
+
+  sprite_fixed_t frog_back;
+  frog_back.ativo = 1;
+  frog_back.coord_x = 200;
+  frog_back.coord_y = 200;
+  frog_back.data_register = 2;
+  frog_back.offset = 1;
+
+  set_fixed_sprite(frog_back);
 
   return 0;
 }
