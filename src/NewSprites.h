709a585aa8e9905077b599796c36b9e0d26d7a14
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-07-08 16:09:58 -0300

    Update NewSprites.h

--- src/NewSprites.h
+++ src/NewSprites.h
@@ -2,6 +2,8 @@
 
 #include "utils/types.h"
 
-void set_new_sprite(char sprite_name, u8_t sprite_offset);
+void set_new_sprite(u8_t sprite_name, u8_t sprite_offset);
 
-u8_t select_sprite(char sprite_name, u8_t pixel_data[400][3]);
+void change_matrix(u8_t matrix[400][3]);
+
+u8_t select_sprite(u8_t sprite_name);
